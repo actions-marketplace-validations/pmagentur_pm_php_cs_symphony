9f340bcc5517493e7f54cc7cb0ef3108da687505
pmAram <54321546+pmAram@users.noreply.github.com>
2020-11-02 15:35:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,10 @@ if [ -z $CODE_PATH ]; then
   STANDARD=.
 fi
 
+if [ -z $MEM_LIMIT ]; then
+  MEM_LIMIT=512M
+fi
 
 
-phpcs -d memory_limit=512M --standard=$STANDARD $ARGS $CODE_PATH
+
+phpcs -d memory_limit=$MEM_LIMIT --standard=$STANDARD $ARGS $CODE_PATH
